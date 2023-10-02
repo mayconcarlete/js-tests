3bad3f5780ed301dfcd861432ee38bd70d769b54
Maycon Carlete <maycon.carlete@gmail.com>
2023-10-02 11:24:20 -0400

    feat: script

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FILES_CHANGED=$(git diff --name-only --diff-filter=ACMRT 04e8044383ecacceffe32a465e46578d323de8d2 00cfbec778b798cd4c577a55279404d658a2427f | xargs)
+FILES_CHANGED=$(git diff --name-only --diff-filter=ACMRT 04e8044383ecacceffe32a465e46578d323de8d2 533244f171c590aaef5176108c13e71ae1ce90cb | xargs)
 PACKAGEJSON=package.json
 PACKAGELOCK=package-lock.json
 if [[ "$FILES_CHANGED" == *"$PACKAGELOCK"* ]]; then
@@ -8,3 +8,6 @@ fi
 echo $FILES_CHANGED
 
 # [[ "$FILES_CHANGED" == *"$PACKAGEJSON"* ]] ||
+
+# to get the sha of the commit
+#git rev-parse feat-test(branch name)

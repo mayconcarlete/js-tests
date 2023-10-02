8fe8cbdc42d32f8de645fe68e7b3fb610c4f55d1
Maycon Carlete <maycon.carlete@gmail.com>
2023-10-02 11:57:21 -0400

    echo branch sha

--- script.sh
+++ script.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-FILES_CHANGED=$(git diff --name-only --diff-filter=ACMRT 04e8044383ecacceffe32a465e46578d323de8d2 533244f171c590aaef5176108c13e71ae1ce90cb | xargs)
-PACKAGEJSON=package.json
-PACKAGELOCK=package-lock.json
-if [[ "$FILES_CHANGED" == *"$PACKAGELOCK"* ]]; then
-  echo "EH SIM"
-fi
-echo $FILES_CHANGED
+BASE_BRANCH=master
+BASE_BRANCH_SHA=$(git rev-parse "$BASE_BRANCH")
+HEAD_BRANCH=feat-test
+HEAD_BRANCH_SHA=$(git rev-parse "$HEAD_BRANCH")
+
+echo "$BASE_BRANCH_SHA" "$HEAD_BRANCH_SHA"
+
+# FILES_CHANGED=$(git diff --name-only --diff-filter=ACMRT 04e8044383ecacceffe32a465e46578d323de8d2 533244f171c590aaef5176108c13e71ae1ce90cb | xargs)
+# PACKAGEJSON=package.json
+# PACKAGELOCK=package-lock.json
+# if [[ "$FILES_CHANGED" == *"$PACKAGELOCK"* ]]; then
+#   echo "EH SIM"
+# fi
+# echo $FILES_CHANGED
 
 # [[ "$FILES_CHANGED" == *"$PACKAGEJSON"* ]] ||
 
